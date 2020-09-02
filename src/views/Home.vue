5c810f5924b9db7b0333cb18dafad59ff41c1ed8
Mehmet Çürükoğlu <mehmetcrkglu@gmail.com>
2020-09-02 20:57:43 +0300

    lesson1 completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Home</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+<script>  
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+
+<style scoped>
+h1 {
+  color:red;
+
+  @media (--t){
+    color:blue
+  }
+}
+</style>
