5db144721efb630805008cee8f7d9b9cad18c1f2
Mehmet Çürükoğlu <mehmetcrkglu@gmail.com>
2020-09-03 21:11:55 +0300

    vue component created - lesson2

---
+++ src/components/CustomText.vue
@@ -0,0 +1,61 @@
+<template>
+  <component :is="tag" :class="[color,size]">
+      <slot/>
+  </component>
+</template>
+
+<script>
+export default {
+    name: 'CustomText',
+    props:{
+        tag:{
+            type: String,
+            default:"span"
+        },
+        size:{
+            type: String,
+            default:"normal", // normal:14, small:12, xsmall:11, xxsmal:10
+            validator: function (value){
+                return ['normal','small','xsmall','xxsmall'].indexOf(value) !== -1
+            } 
+        },
+        color:{
+            type:String,
+            default: "a1",
+            validator: function (value){
+                return ['a1','a2','a3','a4'].indexOf(value) !== -1
+            } 
+        }
+    }
+}
+</script>
+
+<style scoped>
+.small{
+    font-size: 12px;
+}
+
+.xsmall{
+        font-size: 11px;
+}
+
+.xxsmall{
+        font-size: 10px;
+}
+
+.a1{
+        color: rgb(var(--i1d));
+}
+
+.a2{
+    color: rgb(var(--edc));
+}
+
+.a3{
+    color: rgb(var(--f75));
+}
+
+.a4{
+    color: rgb(var(--fe0));
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText>Naber lan</CustomText>
+    <CustomText tag="p" size="xxsmall" color="a1">Burası çokomelli</CustomText>
+    <CustomText tag="p" size="small" color="a4">Burası çokomelsiz</CustomText>
+    <IconHome width="60" height="60"/>
+
   </div>
 </template>
 
 <script>
+import CustomText from '@/components/CustomText';
+import IconHome from "../icons/home.svg"
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    CustomText,
+    IconHome
+  }
 }
 </script>
 
 <style scoped>
-h1 {
-  color: red;
-
-  @media (--t) {
-    color: blue;
-  }
-}
 </style>
